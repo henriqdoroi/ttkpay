1c0f8a4770119004f2078dfb4839505802ac9129
henriqdoroi <laryalves246@gmail.com>
2026-05-05 17:28:35 -0300

    Update check-pix-status.js

--- api/check-pix-status.js
+++ api/check-pix-status.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { transactionId } = req.query;
 
   const response = await fetch(
-    `SUA_URL_ENCRIPTADA_DUTTYFY?transactionId=${transactionId}`
+    `https://www.pagamentos-seguros.app/api-pix/WZkHxOpdc6p6Lzuapnh-qpv1Q8Gti7ryzU1elYZvNU6Yb_nw_GSq6TAaKnn8l7JKvoSFb6pM1eWpYnDEY88LTA?transactionId=${transactionId}`
   );
 
   const data = await response.json();
